dddafe076c8c8b73d41dae0813c41fc31877fd54
Sakib Ansari <152527120+sakibansari546@users.noreply.github.com>
2024-12-18 15:21:22 +0530

    add memories images

--- components/F.tsx
+++ components/F.tsx
@@ -4,11 +4,12 @@
 import React from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
+
 export function Features() {
     return (
         <div className="py-32">
             <h1 className="text-5xl text-black font-bold dark:text-white text-center pb-14">
-                Features
+                Some <span className="text-[#f78764]">Memories</span>
             </h1>
             <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
                 <InfiniteMovingCards
@@ -17,7 +18,7 @@ export function Features() {
                     speed="normal"
                 />
                 <InfiniteMovingCards
-                    items={testimonials}
+                    items={testimonials2}
                     direction="left"
                     speed="normal"
                 />
@@ -28,22 +29,44 @@ export function Features() {
 
 const testimonials = [
     {
-        image: "https://via.placeholder.com/350",
+        image: "/memory/1.jpg",
         name: "Charles Dickens",
         title: "Author of A Tale of Two Cities",
     },
     {
-        image: "https://via.placeholder.com/350",
+        image: "/memory/8.jpg",
         name: "William Shakespeare",
         title: "Author of Hamlet",
     },
     {
-        image: "https://via.placeholder.com/350",
+        image: "/memory/3.jpg",
         name: "Jane Austen",
         title: "Author of Pride and Prejudice",
     },
     {
-        image: "https://via.placeholder.com/350",
+        image: "/memory/4.jpg",
+        name: "Herman Melville",
+        title: "Author of Moby-Dick",
+    },
+];
+const testimonials2 = [
+    {
+        image: "/memory/5.jpg",
+        name: "Charles Dickens",
+        title: "Author of A Tale of Two Cities",
+    },
+    {
+        image: "/memory/6.jpg",
+        name: "William Shakespeare",
+        title: "Author of Hamlet",
+    },
+    {
+        image: "/memory/7.jpg",
+        name: "Jane Austen",
+        title: "Author of Pride and Prejudice",
+    },
+    {
+        image: "/memory/9.jpg",
         name: "Herman Melville",
         title: "Author of Moby-Dick",
     },
