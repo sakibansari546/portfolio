37bc176f1401513c5538e4bc5f31ae482c0a3219
Sakib Ansari <152527120+sakibansari546@users.noreply.github.com>
2024-12-18 15:21:50 +0530

    add memories images

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -54,7 +54,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         >
             <div className="max-w-7xl mx-auto py-12 px-4 md:px-8 lg:px-10">
                 <h2 className="text-2xl font-semibold md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-                    Which Skills I have?
+                    Which <span className="text-[#f78764]">Skills</span> I have?
                 </h2>
                 <p className="text-neutral-700 dark:text-neutral-300 text-md md:text-base max-w-sm">
                     I have experience working on both frontend and backend using various technologies.
